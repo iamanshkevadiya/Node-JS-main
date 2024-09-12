0a1f596e284e69b295ba29c534536e2e3c3e0c9d
Ansh Kevadiya <146837647+iamanshkevadiya@users.noreply.github.com>
2024-09-12 10:03:14 +0530

    Update bookRoutes.js

--- PR – 4 BOOK STORE DETAILS/book_store/routes/bookRoutes.js
+++ PR – 4 BOOK STORE DETAILS/book_store/routes/bookRoutes.js
@@ -1,11 +1,18 @@
 const { Router } = require('express');
-const { getAllBooks, addBooks, deleteBook, updateBook, findBookById } = require('../controllers/bookcontroller');
+const { getAllBooks, createBooks, deleteBook, updateBook, findBookById, upload } = require('../controllers/bookcontroller');
 
 const BookRouter = Router();
-BookRouter.get('/:id',findBookById);
-BookRouter.delete('/delete/:id',deleteBook);
-BookRouter.get('/books',getAllBooks);
-BookRouter.post('/addbooks',addBooks);
-BookRouter.patch('/update/:id',updateBook);
+
+BookRouter.get('/:id', findBookById);
+BookRouter.delete('/delete/:id', deleteBook);
+BookRouter.get('/books', getAllBooks);
+BookRouter.post('/addbooks', upload.single("img"), createBooks);
+BookRouter.patch('/update/:id', updateBook);
+
+BookRouter.post("/upload", upload.single("img"), (req, res) => {
+    console.log("req.file", req.file);
+
+    res.send("file uploaded successfully")
+})
 
 module.exports = BookRouter;
