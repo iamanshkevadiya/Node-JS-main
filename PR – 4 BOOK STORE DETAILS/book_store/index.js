7733d17d84659bc26ed22a16360573c33d00a10b
Ansh Kevadiya <146837647+iamanshkevadiya@users.noreply.github.com>
2024-09-12 10:03:33 +0530

    Update index.js

--- PR – 4 BOOK STORE DETAILS/book_store/index.js
+++ PR – 4 BOOK STORE DETAILS/book_store/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const BookRouter = require('./routes/bookRoutes')
-
+const BookRouter = require('./routes/bookRoutes');
+const path = require('path');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send({ message: "welcome to the book store.!" });
 })
-app.use('/books', BookRouter);
+
+// image
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+app.use('/', BookRouter);
 
 // Start the server
 app.listen(8090, () => {
